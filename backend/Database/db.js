363bdf582d75b281d582fc244a760ab4cfec18d8
Leo-web-7 <richardo.alelo@gmail.com>
2024-11-11 16:50:48 +0700

    fds

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "230523",
-    database: "daftar",
+    database: "todolist",
 });
 
 async function testConnection() {
